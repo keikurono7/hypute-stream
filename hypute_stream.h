403bc17d7f886972e90172905b3814e9321e4179
keikurono7 <125114994+keikurono7@users.noreply.github.com>
2026-06-12 20:22:39 +0530

    Overload hypute_initialize and update hypute_execute

--- hypute_stream.h
+++ hypute_stream.h
@@ -4,14 +4,17 @@
 #include <cstdint>
 
 extern "C" {
-void hypute_initialize();
+
+void hypute_initialize(const char* runtime_context);
+
 void hypute_execute(
-uint64_t block_0,
-uint64_t block_1,
-uint64_t block_2,
-const char* runtime_context
+    uint64_t block_0,
+    uint64_t block_1,
+    uint64_t block_2
 );
+
 void hypute_shutdown();
+
 }
 
 #endif // HYPUTE_STREAM_H
